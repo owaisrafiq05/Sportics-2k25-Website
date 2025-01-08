86df7645865af2a6cffba9b565f2f2b0bcc4c966
saim-x <x_aci@hotmail.com>
2025-01-09 02:46:51 +0500

    Update PersonCard component with image URLs and enhance image styling; modify TeamPage to include images for all team members

--- src/pages/PersonCard.jsx
+++ src/pages/PersonCard.jsx
@@ -5,13 +5,11 @@ export const PersonCard = ({
   name,
   position,
 }) => (
-  <div className=" text-center hover:scale-105 transition-transform shadow-lg rounded-xl p-4 duration-300">
-    <img className="w-48 h-48 rounded-full border-4 border-gray-600 mr-8 ml-8" src={image} />
+  <div className="text-center hover:scale-105 transition-transform shadow-lg rounded-xl p-4 duration-300">
+    <img className="w-48 h-48 rounded-full border-4 border-gray-600 mr-8 ml-8 object-cover" src={image} style={{ objectFit: 'cover' }} />
     <div>
       <h2 className="mt-4 text-gray-600 text-lg font-semibold">{name}</h2>
       <h3 className="text-[#00A8FF]">{position}</h3>
     </div>
   </div>
 );
-
-  
--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -13,24 +13,24 @@ const TeamPage = () => {
 
         <div className="flex flex-col lg:gap-16 md:gap-8 gap-8 flex-wrap">
           <div className='flex justify-center'>
-            <PersonCard image="" name="Taha Abbas" position="President" />
+            <PersonCard image="https://i.ibb.co/x2wd129/president.jpg" name="Taha Abbas" position="President" />
           </div>
           <div className="flex gap-8 lg:gap-16 md:gap-8 justify-center md:flex flex-wrap">
-            <PersonCard image="" name="Ghulam Mohiuddin" position="Vice President" />
-            <PersonCard image="" name="Anush Hussain" position="Vice President" />
+            <PersonCard image="https://i.ibb.co/Xsf9F2c/Vice-President-Ghulam.jpg" name="Ghulam Mohiuddin" position="Vice President" />
+            <PersonCard image="https://i.ibb.co/7SPx6cv/Vice-President-Anush.jpg" name="Anush Hussain" position="Vice President" />
           </div>
           <div className='flex gap-8 lg:gap-16 md:gap-8 flex-wrap justify-center'>
-            <PersonCard image="" name="Wisha Zahid" position="General Secretary" />
-            <PersonCard image="" name="Shahzaib Mirza" position="Treasurer" />
-            <PersonCard image="" name="Zohaib Siddiqui" position="E-Sports Director" />
+            <PersonCard image="https://i.ibb.co/pf1JGGS/Untitled.jpg" name="Wisha Zahid" position="General Secretary" />
+            <PersonCard image="https://i.ibb.co/Xp0kRHJ/treasurer.jpg" name="Shahzaib Mirza" position="Treasurer" />
+            <PersonCard image="https://i.ibb.co/MPZ6f7d/Esports-Head-zohaib.jpg" name="Zohaib Siddiqui" position="E-Sports Director" />
           </div>
           <div className="flex gap-8 lg:gap-16 md:gap-8 flex-wrap justify-center">
-            <PersonCard image="" name="Moeed Uddin" position="Event Manager" />
-            <PersonCard image="" name="Ausaja Hussain" position="Event Manager" />
+            <PersonCard image="https://i.ibb.co/41b4HHN/Event-Manager-Moeed.jpg" name="Moeed Uddin" position="Event Manager" />
+            <PersonCard image="https://i.ibb.co/h1MrPCb/event-manager-ausaja.jpg" name="Ausaja Hussain" position="Event Manager" />
           </div>
           <div className="flex gap-8 lg:gap-16 md:gap-8 flex-wrap justify-center">
-            <PersonCard image="" name="Anser Tayyab" position="Director Discipline Affairs" />
-            <PersonCard image="" name="Almeeza Tariq" position="Director Discipline Affairs" />
+            <PersonCard image="https://i.ibb.co/Zx23dw5/DC-Head-Ansar.jpg" name="Anser Tayyab" position="Director Discipline Affairs" />
+            <PersonCard image="https://i.ibb.co/sKx48qr/dc-head-Almeeza.jpg" name="Almeeza Tariq" position="Director Discipline Affairs" />
           </div>
         </div>
 
